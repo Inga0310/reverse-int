5a12694fa0abc15ed44142b642833bf099d686b3
Inga Katkova <ingakatkova@MacBook-Air-Inga.local>
2022-02-24 12:23:13 +0400

    feat: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        return +(n + '').split('').reverse().join('');
+    } else if (n < 0 ) {
+        return (+ (- n + '').split('').reverse().join(''));
+    }
 }
+
